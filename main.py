b3921d3fb41090c729c0c33b0ff87413c31aa6ee
mtronda21vekby-stack <m.tronda.21vek.by@gmail.com>
2025-12-25 23:11:16 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,4 @@
-# main.py
-# -*- coding: utf-8 -*-
-
-import threading
-import time
-import traceback
-
-from app.log import log
-from app.log import startup_diagnostics
-from app.state import load_state, autosave_loop
-from app.tg import run_telegram_bot_forever
-from app.health import run_http_server
-
+from app.tg import run
 
 if __name__ == "__main__":
-    try:
-        startup_diagnostics()
-        load_state()
-
-        stop_autosave = threading.Event()
-        threading.Thread(target=autosave_loop, args=(stop_autosave, 60), daemon=True).start()
-
-        threading.Thread(target=run_telegram_bot_forever, daemon=True).start()
-        run_http_server()
-
-    except Exception:
-        log.error("FATAL STARTUP ERROR:\n%s", traceback.format_exc())
-        while True:
-            time.sleep(60)
+    run()
