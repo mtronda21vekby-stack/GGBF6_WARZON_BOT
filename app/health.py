536563cd2442230fb626791cf056a3be70cced4c
mtronda21vekby-stack <m.tronda.21vek.by@gmail.com>
2025-12-27 06:53:37 -0500

    Update health.py

--- app/health.py
+++ app/health.py
@@ -1,29 +1,31 @@
 # -*- coding: utf-8 -*-
-from http.server import BaseHTTPRequestHandler, HTTPServer
+import os
 import threading
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
-
-class _H(BaseHTTPRequestHandler):
+class _Handler(BaseHTTPRequestHandler):
     def do_GET(self):
-        if self.path in ("/", "/health", "/healthz"):
+        if self.path in ("/", "/health"):
             self.send_response(200)
             self.send_header("Content-Type", "text/plain; charset=utf-8")
             self.end_headers()
             self.wfile.write(b"ok")
-            return
-        self.send_response(404)
-        self.end_headers()
+        else:
+            self.send_response(404)
+            self.end_headers()
 
-    def log_message(self, fmt, *args):
-        # тихо, чтобы не спамило логи
+    def log_message(self, *_args):
         return
 
+def start_health_server(log):
+    port = int(os.getenv("PORT", "10000"))
+    host = "0.0.0.0"
+    httpd = HTTPServer((host, port), _Handler)
 
-def start_health_server(port: int, log):
     def _run():
-        srv = HTTPServer(("0.0.0.0", port), _H)
-        log.info("Health server listening on :%s", port)
-        srv.serve_forever()
+        log.info("health: listening on %s:%s", host, port)
+        httpd.serve_forever()
 
     t = threading.Thread(target=_run, daemon=True)
     t.start()
+    return t
