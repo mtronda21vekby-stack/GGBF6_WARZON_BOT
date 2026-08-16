3020c8bf229597c4afe466f1747597429bb12454
mtronda21vekby-stack <m.tronda.21vek.by@gmail.com>
2026-08-16 15:58:38 -0400

    feat: add Premium entitlement service package

---
+++ app/services/entitlements/__init__.py
@@ -0,0 +1,11 @@
+from app.services.entitlements.service import (
+    EntitlementStatus,
+    LinkChallenge,
+    PremiumEntitlementService,
+)
+
+__all__ = [
+    "EntitlementStatus",
+    "LinkChallenge",
+    "PremiumEntitlementService",
+]
