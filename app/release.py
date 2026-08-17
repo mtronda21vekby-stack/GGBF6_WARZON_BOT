fd5b11307c8e6a9b51dcb4e00e3fd0e44b1fbe83
mtronda21vekby-stack <m.tronda.21vek.by@gmail.com>
2026-08-16 20:01:55 -0400

    chore(release): bump BLACK CROWN OPS to live intelligence v18

--- app/release.py
+++ app/release.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 from __future__ import annotations
 
-APP_VERSION = "17.0.0"
-RELEASE_CONTRACT = "bco-voice-hifi-v17"
+APP_VERSION = "18.0.0"
+RELEASE_CONTRACT = "bco-live-intelligence-v18"
