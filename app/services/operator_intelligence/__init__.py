e09a7bff4bb8fba5044924b266b68d6ab79bbad2
mtronda21vekby-stack <m.tronda.21vek.by@gmail.com>
2026-08-17 18:21:28 -0400

    Expose v36 orchestrated Operator service across surfaces

--- app/services/operator_intelligence/__init__.py
+++ app/services/operator_intelligence/__init__.py
@@ -1,4 +1,6 @@
 from app.services.operator_intelligence.service import MissionConflict
-from app.services.operator_intelligence.v28 import OperatorIntelligenceService
+from app.services.operator_intelligence.orchestrated_service import (
+    OrchestratedOperatorIntelligenceService as OperatorIntelligenceService,
+)
 
 __all__ = ["MissionConflict", "OperatorIntelligenceService"]
