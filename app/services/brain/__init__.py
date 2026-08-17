0793e64acfc79f0047b4a3298125cead39a1a09a
mtronda21vekby-stack <m.tronda.21vek.by@gmail.com>
2026-08-17 19:03:09 -0400

    Install v38 locale policy at brain package boundary

--- app/services/brain/__init__.py
+++ app/services/brain/__init__.py
@@ -1,1 +1,6 @@
+# BLACK CROWN OPS v38 ecosystem locale policy.
+# Import-time installation is intentional: all BrainEngine/AIHook paths import
+# this package before PromptBuilder is instantiated, preserving one language rule.
+from app.services.brain.locale_patch import install as _install_locale_policy
 
+_install_locale_policy()
