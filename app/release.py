2ae05d7f35c72279cb9b04eef60f43570488c2c9
mtronda21vekby-stack <m.tronda.21vek.by@gmail.com>
2026-08-17 19:04:18 -0400

    Bump BLACK CROWN OPS to v38 ecosystem i18n and analytics

--- app/release.py
+++ app/release.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 from __future__ import annotations
 
-APP_VERSION = "36.0.0"
-RELEASE_CONTRACT = "bco-mission-orchestrator-v36"
+APP_VERSION = "38.0.0"
+RELEASE_CONTRACT = "bco-ecosystem-i18n-admin-analytics-v38"
