82a64b070cb897f491b899727393588435a9a1ba
mtronda21vekby-stack <m.tronda.21vek.by@gmail.com>
2026-08-17 22:53:35 -0400

    Release BLACK CROWN v44 AAA War Room alerts

--- app/release.py
+++ app/release.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 from __future__ import annotations
 
-APP_VERSION = "43.0.1"
-RELEASE_CONTRACT = "bco-personal-meta-baseline-v43.0.1"
+APP_VERSION = "44.0.0"
+RELEASE_CONTRACT = "bco-aaa-war-room-alerts-v44"
