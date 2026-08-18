2488e3a9f04172a257156fc267d7d15db58696fd
mtronda21vekby-stack <m.tronda.21vek.by@gmail.com>
2026-08-17 22:05:48 -0400

    Set CROWN INTEL FREE v42 release contract

--- app/release.py
+++ app/release.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 from __future__ import annotations
 
-APP_VERSION = "41.1.0"
-RELEASE_CONTRACT = "bco-lightweight-site-session-verify-v41.1"
+APP_VERSION = "42.0.0"
+RELEASE_CONTRACT = "bco-crown-intel-free-v42"
