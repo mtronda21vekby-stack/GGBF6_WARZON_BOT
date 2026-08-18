c56240c4e34a9b10ff0226eaf6bc163eec397c54
mtronda21vekby-stack <m.tronda.21vek.by@gmail.com>
2026-08-17 21:54:17 -0400

    Install Telegram console i18n on service import

--- app/services/telegram/__init__.py
+++ app/services/telegram/__init__.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
+from app.services.telegram.console_i18n import install as _install_console_i18n
+
+_install_console_i18n()
+
 from app.services.telegram.command_console import CommandConsoleController
 
 __all__ = ["CommandConsoleController"]
