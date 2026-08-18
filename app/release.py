6d222fa10e7571dfeb81445f58e1307167611b53
mtronda21vekby-stack <m.tronda.21vek.by@gmail.com>
2026-08-17 21:55:53 -0400

    Bump bridge fallback release to v41.1

--- app/release.py
+++ app/release.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 from __future__ import annotations
 
-APP_VERSION = "41.0.0"
-RELEASE_CONTRACT = "bco-site-identity-assertion-v41"
+APP_VERSION = "41.1.0"
+RELEASE_CONTRACT = "bco-lightweight-site-session-verify-v41.1"
