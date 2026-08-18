5c6f4be18f6dcd5a4c4cb063d269ec6b2a4e12ab
mtronda21vekby-stack <m.tronda.21vek.by@gmail.com>
2026-08-17 22:22:26 -0400

    Bump BLACK CROWN release to v43 Personal Meta

--- app/release.py
+++ app/release.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 from __future__ import annotations
 
-APP_VERSION = "42.0.0"
-RELEASE_CONTRACT = "bco-crown-intel-free-v42"
+APP_VERSION = "43.0.0"
+RELEASE_CONTRACT = "bco-personal-meta-v43"
